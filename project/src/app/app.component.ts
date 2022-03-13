a3fb2928e54ab7f30efe7b0f0460a95928b905ed
RahimliZakir <zakirer@code.edu.az>
2022-03-14 02:31:23 +0400

    Reactive Forms Logic Done

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'appName';
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  //* For Form & Inputs (Grouped)
+  identifyForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  //* FormBuilder Using
+  // identifyForm = this.formBuilder.group({
+  //   name: [''],
+  //   email: [''],
+  //   phone: [''],
+  //   message: [''],
+  // });
+
+  //* For One Input
+  // name = new FormControl('');
+
+  // setNameValue(): void {
+  //   this.name.setValue('Hello World!');
+  // }
 }
