3c34910ad00ef168bdb64cca362d086ea3945d83
RahimliZakir <zakirer@code.edu.az>
2022-05-03 03:56:49 +0400

    Validators.pattern Added

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -20,7 +20,16 @@ export class AppComponent {
   //* For Form & Inputs (Grouped)
   identifyForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [
+      Validators.required,
+      // Validators.email,
+      // with RegExp
+      // Validators.pattern(new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)),
+      // with string pattern (eger "string" tipinde yaziriqsa "/ /"-leri silmeli ve "\"-leri 2 eded yazmaliyiq)
+      Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+      // Validators.email yazmishiqsa zaten o bizden email tipli deyer gozleyir amma eger istesek elave
+      // ReGex pattern-i de yaza bilerik.
+    ]),
     phone: new FormControl('', [
       Validators.required,
       Validators.pattern(''),
@@ -30,6 +39,8 @@ export class AppComponent {
       Validators.required,
       Validators.minLength(15),
     ]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
   });
 
   //* FormBuilder Using
